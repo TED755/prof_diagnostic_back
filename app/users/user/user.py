c459048cdb79bca75254cc19c3e56af4a9ee2e9f
Евгений Тихомиров <zhunek@MacBook-Air-Evgenij.local>
2023-02-16 21:09:56 +0300

    first commit

---
+++ app/users/user/user.py
@@ -0,0 +1,8 @@
+class User():
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email  = email
+        self.password = password
+
+    # def get_email(self):
+    #     return self.email
